7c18bd9819dc35e45cb85039e32fd04f3ad12247
alexalexey7797-a11y <alexalexey7797@gmail.com>
2026-09-02 13:34:41 -0200

    Update server.py
    dsds

--- server.py
+++ server.py
@@ -1,1 +1,19 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "BEHRUZBEK AI server ishlayapti! 🤖"
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    data = request.json
+    message = data.get("message", "")
+
+    return jsonify({
+        "reply": f"Sening xabaring: {message}"
+    })
+
+if __name__ == "__main__":
+    app.run()
